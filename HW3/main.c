d4ad3a83379ca8164d4013ed941693fade99a727
B1229023 <144075737+B1229023@users.noreply.github.com>
2024-04-03 23:47:17 +0800

    Update main.c

--- HW3/main.c
+++ HW3/main.c
@@ -114,3 +114,133 @@ int main() {
     }
     fclose(rc);
 }
+/*不知道需不需要重複執行輸入樂透，直到輸入0兌獎，所以就再放一個程式碼了
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+    int receipt_id;
+    int receipt_price;
+    int lotto_set[5][7];
+} lotto_record_t;
+
+void print_info(int lotto[]) {
+    printf("號碼：%02d %02d %02d %02d %02d %02d %02d\n", lotto[0], lotto[1], lotto[2], lotto[3], lotto[4], lotto[5], lotto[6]);
+}
+
+int main() {
+    srand(time(NULL));
+    int n;
+    do {
+        printf("請問要買幾組樂透(輸入0進行兌獎)：");
+        scanf("%d", &n);
+        if (n == 0) {
+            break;
+        }
+
+        char filename[20];
+        FILE* rc = fopen("record.bin", "ab+");
+        
+        if (n != 0) {
+            int num[7];
+            lotto_record_t lotto_record;
+            lotto_record.receipt_price = n * 100;
+
+            if (fopen("record.bin", "rb") == NULL) {
+                lotto_record.receipt_id = 1;
+            }
+            else {
+                fseek(rc, -sizeof(lotto_record_t), SEEK_END);
+                fread(&lotto_record.receipt_id, sizeof(int), 1, rc);
+            }
+            sprintf(filename, "lotto%05d.txt", ++lotto_record.receipt_id);
+            FILE* fp = fopen(filename, "w");
+
+            fprintf(fp, "========= lotto649 =========\n");
+            fprintf(fp, "========+ No.%05d +========\n", lotto_record.receipt_id);
+            time_t curtime;
+            time(&curtime);
+            fprintf(fp, "%s\n",ctime(&curtime));
+            for (int j = 1; j <= 5; j++) {
+                fprintf(fp, "[%d]: ", j);
+                if (j > n) {
+                    fprintf(fp, "__ __ __ __ __ __ __\n");
+                }
+                else {
+                    for (int i = 0; i < 7; i++) {
+                        num[i] = rand() % 69 + 1;
+                        for (int k = 0; k < i; k++) {
+                            if (num[i] == num[k]) {
+                                i--;
+                                break;
+                            }
+                        }
+                        lotto_record.lotto_set[j - 1][i] = num[i];
+                    }
+                    fprintf(fp, "%02d %02d %02d %02d %02d %02d %02d\n", num[0], num[1], num[2], num[3], num[4], num[5], num[6]);
+                }
+            }
+            fprintf(fp, "========= csie@CGU =========\n");
+            fwrite(&lotto_record, sizeof(lotto_record_t), 1, rc);
+
+            fclose(fp);
+        }
+        fclose(rc);
+    } while (n != 0);
+
+    // 兌獎模式
+    if (n == 0) {
+        int check_num[3] = {0}, m = 0, lotto_num = 1;
+        printf("請輸入中獎號碼(最多三個)：");
+        do {
+            scanf("%d", &check_num[m++]);
+            if (m >= 3) break;
+        } while (getchar() != '\n');
+
+        printf("輸入中獎號碼為：");
+        for (int i = 0; i < 3; i++) {
+            if (check_num[i] != '\0') printf("%02d ", check_num[i]);
+        }
+        printf("\n以下為中獎彩券：\n");
+        FILE* rc = fopen("record.bin", "rb");
+        fseek(rc, 0, SEEK_END);
+        int total = ftell(rc) / sizeof(lotto_record_t);
+
+        while (lotto_num <= total) {
+            int correct_arr[5] = {0};
+            int c = 1;
+            lotto_record_t lotto_record_read;
+            fseek(rc, sizeof(lotto_record_t) * (lotto_num - 1), SEEK_SET);
+            fread(&lotto_record_read, sizeof(lotto_record_t), 1, rc);
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 7; j++) {
+                    for (int k = 0; k < 3; k++) {
+                        if (check_num[k] == 0) {
+                            continue;
+                        }
+                        else if (check_num[k] == lotto_record_read.lotto_set[i][j]) {
+                            correct_arr[i] = 1;
+                        }
+                    }
+                }
+                if (c == 1 && correct_arr[0] + correct_arr[1] + correct_arr[2] + correct_arr[3] + correct_arr[4] > 0) {
+                    printf("彩券No.%d\n", lotto_num);
+                    c = 0;
+                }
+                for (int j = 0; j < 5; j++) {
+                    if (correct_arr[i] == 1) {
+                        print_info(lotto_record_read.lotto_set[i]);
+                        break;
+                    }
+                }
+            }
+            lotto_num++;
+        }
+        fclose(rc);
+    }
+
+    return 0;
+}
+*/
